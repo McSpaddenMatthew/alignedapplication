7b88a486b99473d9470016b613c318423c981ed6
McSpaddenMatthew <mcspaddenmatthew@gmail.com>
2025-08-04 13:00:35 -0400

    Create index.tsx

---
+++ pages/pages/index.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
+      <h1>Welcome to Aligned 👋</h1>
+      <p>This is your homepage. Go to /submit to try the form.</p>
+    </div>
+  );
+}
