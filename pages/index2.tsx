6a94b1cdfd90d2323f00c1192e8a3e10d18dbaf5
McSpaddenMatthew <mcspaddenmatthew@gmail.com>
2025-08-04 13:18:50 -0400

    try

--- pages/index2.tsx
+++ pages/index2.tsx
@@ -1,8 +1,1 @@
-export default function Home() {
-  return (
-    <div style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
-      <h1>Welcome to Aligned!</h1>
-      <p>This is your homepage.</p>
-    </div>
-  );
-}
+
